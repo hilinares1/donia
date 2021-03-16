a7a0fe7c71c357e0b546a3c5e985154d8924516c
marcosmichel89 <mmartinezp@webcuba.es>
2021-03-16 09:42:40 -0400

    fix

--- oct_clean_excel/models/models.py
+++ oct_clean_excel/models/models.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class oct_clean_excel(models.Model):
-#     _name = 'oct_clean_excel.oct_clean_excel'
-#     _description = 'oct_clean_excel.oct_clean_excel'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+class oct_clean_excel(models.Model):
+    _name = 'oct.clean.excel'
