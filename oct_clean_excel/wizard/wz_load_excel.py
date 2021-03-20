04193f1aa6dce5b6c1c5fc36b35ec630ea75dff6
marcosmichel89 <mmartinezp@webcuba.es>
2021-03-19 21:51:38 -0400

    some fix

--- oct_clean_excel/wizard/wz_load_excel.py
+++ oct_clean_excel/wizard/wz_load_excel.py
@@ -25,61 +25,69 @@ class LoadExcelFile(models.TransientModel):
 
     def load_excel_file(self):
 
-        if self.col_attribute == None or self.col_attribute_value == None:
+        if not self.col_attribute or not self.col_attribute_value:
             raise exceptions.Warning("Los campos [Columna de atributo] y [Columna de Valor] son obligatorios")
         else:
+            if not self.serial_file:
+                raise exceptions.Warning("Debe seleccionar un archivo para modificar")
+            else:
 
-            colattr = self.col_attribute          # columna 4 atributos
-            colattrv = self.col_attribute_value   # columna 5 valores de los atributos
+                try:
+                    colattr = self.col_attribute  # columna 4 atributos
+                    colattrv = self.col_attribute_value  # columna 5 valores de los atributos
 
-            final_name = "modified_file_" + self.generate_random_name()
+                    final_name = "modified_file_" + self.generate_random_name()
 
-            with tempfile.NamedTemporaryFile(prefix=final_name, suffix='.xlsx', delete=False) as tmp:
-                tmp.write(base64.decodebytes(self.serial_file))
+                    with tempfile.NamedTemporaryFile(prefix=final_name, suffix='.xlsx', delete=False) as tmp:
+                        tmp.write(base64.decodebytes(self.serial_file))
 
-            xfile = openpyxl.load_workbook(tmp.name)
-            sheet = xfile.worksheets[0]
+                    xfile = openpyxl.load_workbook(tmp.name)
+                    sheet = xfile.worksheets[0]
 
-            row_count = sheet.max_row
+                    row_count = sheet.max_row
 
-            row_with_value = 0
+                    row_with_value = 0
 
-            for row in range(1, row_count + 1):
-                if not sheet.cell(row, 1).value == None:
-                    row_with_value = row
-                else:
+                    for row in range(1, row_count + 1):
+                        if not sheet.cell(row, 1).value == None:
+                            row_with_value = row
+                        else:
 
-                    if sheet.cell(row, colattr).value == None or sheet.cell(row, colattr).value.strip() == sheet.cell(
-                            row_with_value, colattr).value.strip():
+                            if sheet.cell(row, colattr).value == None or sheet.cell(row,
+                                                                                    colattr).value.strip() == sheet.cell(
+                                    row_with_value, colattr).value.strip():
 
-                        sheet.cell(row_with_value, colattrv).value = sheet.cell(row_with_value,
-                                                                         colattrv).value.strip() + ',' + sheet.cell(row,
-                                                                                                             colattrv).value.strip()
-                        sheet.cell(row, colattrv).value = ""
-                        sheet.cell(row, 1).value = 'delete'
-                    else:
-                        row_with_value = row
+                                sheet.cell(row_with_value, colattrv).value = sheet.cell(row_with_value,
+                                                                                        colattrv).value.strip() + ',' + sheet.cell(
+                                    row,
+                                    colattrv).value.strip()
+                                sheet.cell(row, colattrv).value = ""
+                                sheet.cell(row, 1).value = 'delete'
+                            else:
+                                row_with_value = row
 
-            xfile.save(tmp.name)
-            self.clean_file_deleted_row(tmp)
+                    xfile.save(tmp.name)
+                    self.clean_file_deleted_row(tmp)
 
-            with open(tmp.name, 'rb') as r:
-                file = base64.b64encode(r.read())
+                    with open(tmp.name, 'rb') as r:
+                        file = base64.b64encode(r.read())
 
-            att_vals = {
-                'name': final_name + '.xlsx',
-                'type': 'binary',
-                'datas': file,
-            }
+                    att_vals = {
+                        'name': final_name + '.xlsx',
+                        'type': 'binary',
+                        'datas': file,
+                    }
 
-            attachment_id = self.env['ir.attachment'].create(att_vals)
-            self.env.cr.commit()
+                    attachment_id = self.env['ir.attachment'].create(att_vals)
+                    self.env.cr.commit()
 
-            return {
-                'type': 'ir.actions.act_url',
-                'url': '/web/content/{}?download=true'.format(attachment_id.id, ),
-                'target': 'self',
-            }
+                    return {
+                        'type': 'ir.actions.act_url',
+                        'url': '/web/content/{}?download=true'.format(attachment_id.id, ),
+                        'target': 'self',
+                    }
+                except Exception:
+                    raise exceptions.Warning("El archivo seleccionado parece no ser válido")
 
     def clean_file_deleted_row(self, tmp):
         xfile = openpyxl.load_workbook(tmp.name)
