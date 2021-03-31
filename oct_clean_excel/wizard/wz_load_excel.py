cdbb6bf419405caab384e10b4c11fb641a8654e2
marcosmichel89 <mmartinezp@webcuba.es>
2021-03-31 10:11:51 -0400

    some fix

--- oct_clean_excel/wizard/wz_load_excel.py
+++ oct_clean_excel/wizard/wz_load_excel.py
@@ -16,15 +16,16 @@ class LoadExcelFile(models.TransientModel):
     _description = 'Model to import and clean data'
 
     serial_file = fields.Binary(string="Load File")
+
     col_attribute = fields.Integer(string='Attribute column')
     col_attribute_value = fields.Integer(string='Column of value')
+
     col_id_attribute = fields.Integer(string='Column of Attributte ID')
     col_id_attribute_value = fields.Integer(string='Column of Attribute Value')
 
     def generate_random_name(self):
         letters = string.ascii_lowercase
         return ''.join(random.choice(letters) for i in range(10))
-
 
     def load_excel_file(self):
         if not self.col_attribute or not self.col_attribute_value:
@@ -34,62 +35,77 @@ class LoadExcelFile(models.TransientModel):
                 raise exceptions.Warning(_("You must select a file to modify"))
             else:
 
-                try:
-                    colattr = self.col_attribute  # columna 4 atributos
-                    colattrv = self.col_attribute_value  # columna 5 valores de los atributos
+                colattr = self.col_attribute  # columna 4 atributos
+                colattrv = self.col_attribute_value  # columna 5 valores de los atributos
 
-                    final_name = "modified_file_" + self.generate_random_name()
+                final_name = "modified_file_" + self.generate_random_name()
 
-                    with tempfile.NamedTemporaryFile(prefix=final_name, suffix='.xlsx', delete=False) as tmp:
-                        tmp.write(base64.decodebytes(self.serial_file))
+                with tempfile.NamedTemporaryFile(prefix=final_name, suffix='.xlsx', delete=False) as tmp:
+                    tmp.write(base64.decodebytes(self.serial_file))
 
-                    xfile = openpyxl.load_workbook(tmp.name)
-                    sheet = xfile.worksheets[0]
+                xfile = openpyxl.load_workbook(tmp.name)
+                sheet = xfile.worksheets[0]
 
-                    row_count = sheet.max_row
+                row_count = sheet.max_row
 
-                    row_with_value = 0
+                row_with_value = 0
 
-                    for row in range(1, row_count + 1):
-                        if not sheet.cell(row, 1).value == None:
-                            row_with_value = row
-                        else:
+                # atributos, valores = self.get_map_attribute_value()
 
-                            if sheet.cell(row, colattr).value == None or sheet.cell(row,
-                                                                                    colattr).value.strip() == sheet.cell(
-                                row_with_value, colattr).value.strip():
+                attr_object = self.env['product.attribute'].search([])
 
-                                sheet.cell(row_with_value, colattrv).value = sheet.cell(row_with_value,
-                                                                                        colattrv).value.strip() + ',' + sheet.cell(
-                                    row,
-                                    colattrv).value.strip()
+                for row in range(1, row_count + 1):
+                    if not sheet.cell(row, 1).value == None:
+                        row_with_value = row
+                    else:
+
+                        if sheet.cell(row, colattr).value == None or sheet.cell(row,
+                                                                                colattr).value.strip() == sheet.cell(
+                            row_with_value, colattr).value.strip():
+
+                            if not str(sheet.cell(row, colattrv).value) == 'None':
+                                sheet.cell(row_with_value, colattrv).value = str(
+                                    sheet.cell(row_with_value, colattrv).value) + ',' + str(
+                                    sheet.cell(row, colattrv).value)
                                 sheet.cell(row, colattrv).value = ""
                                 sheet.cell(row, 1).value = 'delete'
-                            else:
-                                row_with_value = row
+                        else:
+                            row_with_value = row
 
-                    xfile.save(tmp.name)
-                    self.clean_file_deleted_row(tmp)
+                xfile.save(tmp.name)
+                self.clean_file_deleted_row(tmp)
 
-                    with open(tmp.name, 'rb') as r:
-                        file = base64.b64encode(r.read())
+                self.put_id_into_excel(tmp)
 
-                    att_vals = {
-                        'name': final_name + '.xlsx',
-                        'type': 'binary',
-                        'datas': file,
-                    }
+                #                     if not sheet.cell(row1, colattrv).value == '' and not sheet.cell(row1, colattrv).value == None:
 
-                    attachment_id = self.env['ir.attachment'].create(att_vals)
-                    self.env.cr.commit()
+                #                         value_obj = self.env['product.attribute.value'].search([('attribute_id', '=', attr_obj.id)])
 
-                    return {
-                        'type': 'ir.actions.act_url',
-                        'url': '/web/content/{}?download=true'.format(attachment_id.id, ),
-                        'target': 'self',
-                    }
-                except Exception:
-                    raise exceptions.Warning(_("The selected file seems to be not valid"))
+                #                         for value in sheet.cell(row1, colattrv).value.split(','):
+                #                             for val in value_obj:
+                #                                 if val.name == value:
+                #                                     if sheet.cell(row1, self.col_id_attribute_value).value == '' or sheet.cell(row1, self.col_id_attribute_value).value == None:
+                #                                         sheet.cell(row1, self.col_id_attribute_value).value = val.id
+                #                                     else:
+                #                                         sheet.cell(row1, self.col_id_attribute_value).value = str(sheet.cell(row1, self.col_id_attribute_value).value) + ',' + str(val.id)
+
+                with open(tmp.name, 'rb') as r:
+                    file = base64.b64encode(r.read())
+
+                att_vals = {
+                    'name': final_name + '.xlsx',
+                    'type': 'binary',
+                    'datas': file,
+                }
+
+                attachment_id = self.env['ir.attachment'].create(att_vals)
+                self.env.cr.commit()
+
+                return {
+                    'type': 'ir.actions.act_url',
+                    'url': '/web/content/{}?download=true'.format(attachment_id.id, ),
+                    'target': 'self',
+                }
 
     def clean_file_deleted_row(self, tmp):
         xfile = openpyxl.load_workbook(tmp.name)
@@ -102,5 +118,42 @@ class LoadExcelFile(models.TransientModel):
             for row1 in range(row, row_count + 1):
                 if sheet.cell(row1, 1).value == 'delete':
                     sheet.delete_rows(row1, 1)
+
+        xfile.save(tmp.name)
+
+    def put_id_into_excel(self, tmp):
+
+        xfile = openpyxl.load_workbook(tmp.name)
+        sheet = xfile.worksheets[0]
+
+        row_count = sheet.max_row
+        column_count = sheet.max_column
+
+        colattr = self.col_attribute  # columna 4 atributos
+        colattrv = self.col_attribute_value  # columna 5 valores de los atributos
+
+        for row in range(2, row_count + 1):
+
+            attr_obj = self.env['product.attribute'].search([('name', '=', sheet.cell(row, colattr).value)])
+
+            if attr_obj:
+                sheet.cell(row, self.col_id_attribute).value = str(attr_obj.id)
+
+            if not sheet.cell(row, self.col_attribute_value).value == '' and not sheet.cell(row,
+                                                                                            self.col_attribute_value).value == None and not sheet.cell(
+                row, self.col_attribute_value).value == 'None':
+                values = sheet.cell(row, self.col_attribute_value).value.split(',')
+            for val in values:
+                value_obj = self.env['product.attribute.value'].search([('attribute_id', '=', attr_obj.id)])
+                for obj in value_obj:
+                    if obj.name == val:
+                        if sheet.cell(row, self.col_id_attribute).value == None:
+                            sheet.cell(row, self.col_id_attribute).value = obj.id
+                        else:
+                            sheet.cell(row, self.col_id_attribute).value = ',' + str(attr_obj.id)
+
+                # _logger.info("====== valor del atributo=== : %r " % value_obj.name)
+
+        # _logger.info("========= VALUES ========= %r " % values)
 
         xfile.save(tmp.name)
